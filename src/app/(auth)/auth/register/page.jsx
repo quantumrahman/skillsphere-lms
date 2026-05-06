d7550ba63adf02d5607a3d03bf34e861812d2c71
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-07 05:45:50 +0600

    feat: implement register form in register page

--- src/app/(auth)/auth/register/page.jsx
+++ src/app/(auth)/auth/register/page.jsx
@@ -1,8 +1,28 @@
+import RegisterForm from "@/components/ui/Form/RegisterForm/RegisterForm";
+import FormLogo from "@/components/ui/Logos/FormLogo/FormLogo";
+import Link from "next/link";
+
 const RegisterPage = () => {
 
     return (
         <div className="w-full min-h-screen flex items-center justify-center">
-            <h1>Register Page</h1>
+            <div className="w-full max-w-[350px]">
+                <FormLogo />
+                <div className="w-full mt-[30px]">
+                    <RegisterForm />
+                </div>
+                <div className="w-full h-[22px] mt-5 text-center">
+                    <span className="text-sm font-normal text-[#6C706F]">
+                        Already got an account?
+                        <Link 
+                            href={"/auth/login"}
+                            className="w-fit text-sm font-normal text-[#6C706F] ml-1 underline"
+                        >
+                            Login
+                        </Link>
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
